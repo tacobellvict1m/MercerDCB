aa19398e627d75b0f48f073167ae01cf495e0094
tacobellvict1m <taylorvalliere379@gmail.com>
2026-08-19 23:35:16 -0500

    Update journal-data.js

--- journal-data.js
+++ journal-data.js
@@ -1,1 +1,20 @@
 
+const journalEntries = [
+
+    {
+        date: "08-19-2026",
+        title: "Nobody Listens",
+        mood: "Frustrated",
+
+        text: `
+I've been feeling more avoided lately. I don't know if people are actually avoiding me or if it's just how I'm seeing things, but either way, it doesn't feel good. I try to be around people, try to speak up, and try to take responcibility, but it feels like I'm always standing a few steps behind everyone else. I'm supposed to be someone people can look toward for leadership. The problem is, I don't always feel like I know what I'm doing.
+
+I know I need to step up. I know I need to become a better leader and stop waiting for someone else to make the decisions for me. But I also know I need guidance. That's the part I'm struggling with the most. There are people around me who could probably help, but every time I think about asking, I get nervous. I don't want them thinking I'm incapable or that I don't deserve my position. So instead, I keep everything to myself and try to figure it out alone, even when I know that's probably making things harder. It takes a lot off courage for me to say something like ask how somebodies day is going.
+
+I think that's part of the reason on me not getting Vanguard Co-Lead still bothers me so much. I won't lie about it—I was really disappointed. I genuinely believed I had earned that position, and a part of me still feels like I deserved it more than Aquila. Maybe I shouldn't feel that way, but I can't just turn it off. I've tried to put in the work, and I've tried to prove that I'm capable of leading. When I wasn't chosen, it made me wonder if any of that was actually noticed.
+
+Maybe I'm looking at this the wrong way. Maybe I shouldn't be worrying about the position I didn't get and should instead focus on becoming the person who deserves the next one. I just wish I didn't have to figure that out alone. I need someone to guide me, but I admit that I need help.
+
+For now, I guess all I can do is keep showing up and keep trying. Maybe eventually people will see as someone worth following. until then, I'll have to keep pushing forward, even if I don't completely know where I'm going.
+`
+];
